cf095d6d6cb63537c915839628b7fb2c8cdae184
SamvelRaja Sakthivel <sam@SamvelRajas-MacBook-Pro.local>
2023-09-01 11:24:38 +0530

    Fixed Security evaluation recommendations

--- packages/cosmos-snap-provider/src/snap.ts
+++ packages/cosmos-snap-provider/src/snap.ts
@@ -5,12 +5,73 @@ import { defaultSnapOrigin } from './config';
 import { ChainInfo, GetSnapsResponse, Snap } from './types';
 
 /**
+ * The fool proof version of getting the ethereum provider suggested by
+ * https://github.com/Montoya/snap-connect-test/blob/0dad2dd53ab2ecbf4b4369230d3aaaeca08c6dae/index.html#L41
+ * @returns the ethereum provider which supports snaps 
+ */
+const getProvider = async (): Promise<any> => {
+  let mmFound = false; 
+  if ('detected' in window.ethereum) {
+    for (const provider of window.ethereum.detected) {
+      try {
+        // Detect snaps support
+        await provider.request({
+          method: 'wallet_getSnaps',
+        });
+        // enforces MetaMask as provider 
+        window.ethereum.setProvider(provider);
+
+        mmFound = true; 
+        return provider;
+      } catch {
+        // no-op
+      }
+    }
+  }
+
+  if(!mmFound && 'providers' in window.ethereum) { 
+    for (const provider of window.ethereum.providers) { 
+      try {
+        // Detect snaps support
+        await provider.request({
+          method: 'wallet_getSnaps',
+        });
+        
+        window.ethereum = provider; 
+
+        mmFound = true; 
+        return provider;
+      } catch {
+        // no-op
+      }
+    }
+  }
+
+  return window.ethereum;
+};
+
+const sendReqToSnap = async (method: string, params:any): Promise<any> =>  {
+  const provider =  await getProvider();
+  return provider.request({
+    method: 'wallet_invokeSnap',
+    params: {
+      snapId: defaultSnapOrigin,
+      request: {
+        method: method,
+        params: params,
+      },
+    },
+  });
+}
+
+/**
  * Get the installed snaps in MetaMask.
  *
  * @returns The snaps installed in MetaMask.
  */
 export const getSnaps = async (): Promise<GetSnapsResponse> => {
-  return (await window.ethereum.request({
+  const provider =  await getProvider();
+  return (await provider.request({
     method: 'wallet_getSnaps',
   })) as unknown as GetSnapsResponse;
 };
@@ -25,7 +86,8 @@ export const connectSnap = async (
   snapId: string = defaultSnapOrigin,
   params: Record<'version' | string, unknown> = {},
 ) => {
-  await window.ethereum.request({
+  const provider =  await getProvider();
+  provider.request({
     method: 'wallet_requestSnaps',
     params: {
       [snapId]: params,
@@ -62,19 +124,10 @@ export const requestSignature = async (
     accountNumber?: Long | null;
   },
 ) => {
-  const signature = await window.ethereum.request({
-    method: 'wallet_invokeSnap',
-    params: {
-      snapId: defaultSnapOrigin,
-      request: {
-        method: 'signDirect',
-        params: {
-          chainId,
-          signerAddress,
-          signDoc,
-        },
-      },
-    },
+  const signature = await sendReqToSnap('signDirect', {
+      chainId,
+      signerAddress,
+      signDoc,
   });
 
   const { accountNumber } = signDoc;
@@ -107,37 +160,20 @@ export const requestSignAmino = async (
   signDoc: StdSignDoc,
   { isADR36 = false } = {},
 ) => {
-  const signResponse = (await window.ethereum.request({
-    method: 'wallet_invokeSnap',
-    params: {
-      snapId: defaultSnapOrigin,
-      request: {
-        method: 'signAmino',
-        params: {
-          chainId,
-          signerAddress,
-          signDoc,
-          isADR36,
-        },
-      },
-    },
-  })) as AminoSignResponse;
+  const signResponse = (await sendReqToSnap('signAmino', {
+    chainId,
+    signerAddress,
+    signDoc,
+    isADR36,
+  }))as AminoSignResponse;
 
   return signResponse;
 };
 
 export const getKey = async (chainId: string): Promise<AccountData> => {
-  const accountData = await window.ethereum.request({
-    method: 'wallet_invokeSnap',
-    params: {
-      snapId: defaultSnapOrigin,
-      request: {
-        method: 'getKey',
-        params: {
-          chainId,
-        },
-      },
-    },
+
+  const accountData = await sendReqToSnap('getKey', {
+    chainId,
   });
 
   if (!accountData) {
@@ -154,17 +190,8 @@ export const isLocalSnap = (snapId: string) => snapId.startsWith('local:');
 export const suggestChain = async (
   chainInfo: ChainInfo,
 ): Promise<{ message: string; chainInfo: ChainInfo }> => {
-  return await window.ethereum.request({
-    method: 'wallet_invokeSnap',
-    params: {
-      snapId: defaultSnapOrigin,
-      request: {
-        method: 'suggestChain',
-        params: {
-          chainInfo,
-        },
-      },
-    },
+  return await sendReqToSnap('suggestChain', {
+    chainInfo,
   });
 };
 
