ee5b6a7dbf21c65ec7d00c4f40929d9c5b2ea6da
baryon2 <opthami@gmail.com>
2023-09-08 15:22:51 +0200

    fix connect function in snap provider

--- packages/cosmos-snap-provider/src/snap.ts
+++ packages/cosmos-snap-provider/src/snap.ts
@@ -89,7 +89,7 @@ export const connectSnap = async (
   params: Record<'version' | string, unknown> = {},
 ) => {
   const provider = await getProvider();
-  provider.request({
+  await provider.request({
     method: 'wallet_requestSnaps',
     params: {
       [snapId]: params,
